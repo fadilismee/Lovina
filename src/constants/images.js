54b73b22bbf96e3a529908930c78da7930f78e8c
fadil ie <168767584+fadilismee@users.noreply.github.com>
2024-10-26 21:35:32 +0700

    first

---
+++ src/constants/images.js
@@ -0,0 +1,51 @@
+import Img1 from "../assets/pics/1.png";
+import Img2 from "../assets/pics/2.png";
+import Img3 from "../assets/pics/3.png";
+import Img4 from "../assets/pics/4.png";
+import Img5 from "../assets/pics/5.png";
+import Img6 from "../assets/pics/13.png";
+import Img7 from "../assets/pics/8.png";
+import Img8 from "../assets/pics/7.png";
+import Img9 from "../assets/pics/10.png";
+import Img10 from "../assets/pics/11.png";
+import Img11 from "../assets/pics/12.png";
+import Img12 from "../assets/pics/6.png";
+
+export const images = [
+  {
+    src: Img1,
+  },
+  {
+    src: Img2,
+  },
+  {
+    src: Img3,
+  },
+  {
+    src: Img4,
+  },
+  {
+    src: Img5,
+  },
+  {
+    src: Img6,
+  },
+  {
+    src: Img7,
+  },
+  {
+    src: Img8,
+  },
+  {
+    src: Img9,
+  },
+  {
+    src: Img10,
+  },
+  {
+    src: Img11,
+  },
+  {
+    src: Img12,
+  },
+];
